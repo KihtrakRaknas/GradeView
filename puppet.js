4f6362cd8b2ff47cd712d4b4ed61e5a1c3aaf5e4
Karthik Sankar <karthikandsankar@gmail.com>
2019-02-05 12:49:22 -0500

    Update

--- puppet.js
+++ puppet.js
@@ -2,12 +2,13 @@ const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const url = 'https://students.sbschools.org/genesis/parents?gohome=true';
 
-var email = '10013096@sbstudents.org';
-var pass = '';
-var url2 = encodeURI('https://students.sbschools.org/genesis/j_security_check?j_username='+email+'&j_password='+pass);
+var id = '10012734'
+var email = encodeURIComponent(id+'@sbstudents.org');
+var pass = encodeURIComponent('Sled%2#9');
+var url2 = 'https://students.sbschools.org/genesis/j_security_check?j_username='+email+'&j_password='+pass;
 
 function func(){
-    eval("header_goToTab('studentdata&tab2=gradebook','studentid=10013096');");
+    eval("header_goToTab('studentdata&tab2=gradebook','studentid="+id+"');");
 }
 
 
@@ -17,6 +18,11 @@ function func(){
         slowMo: 250, // slow down puppeteer script so that it's easier to follow visually
       });
     const page = await browser.newPage();
+
+    await page.setViewport({
+	    width: 1920,
+	    height: 1080
+	})
 
     /*page.on('request', request => {
       if (request.resourceType() === 'image')
@@ -40,20 +46,20 @@ function func(){
         navresponse = page.waitForNavigation(['networkidle0', 'load', 'domcontentloaded']);
         await page.evaluate(text => [...document.querySelectorAll('*')].find(e => e.textContent.trim() === text).click(), "Gradebook");
         await navresponse
-        
+
         navresponse = page.waitForNavigation(['networkidle0', 'load', 'domcontentloaded']);
         const currentMarking = await page.evaluate( () => ((document.querySelectorAll( '[name="fldMarkingPeriod"]')[0]).value));
-        
+
         if(currentMarking!=period){
           console.log("diff"+currentMarking+":"+period)
           await page.evaluate((markingPeriod) => switchMarkingPeriod(markingPeriod),period);
           console.log("switch")
           await navresponse
-          
+
         }
 
         const html = await page.content();
-      
+
         await page.screenshot({path: period+'examples.png'});
         var title
         await $('.list', html).find("tbody").find(".categorytab").each(function() {
@@ -66,7 +72,7 @@ function func(){
             console.log(teacherName);
             if(!grades[className]["teacher"])
               grades[className]["teacher"]=teacherName;
-              
+
 
               //var avg = $(this).parent().parent().find($("td[title='View Course Summary']")).textContent;
               var avg = $(this).parent().parent().find(".cellRight").eq(0).text().trim();
@@ -79,15 +85,21 @@ function func(){
 
 
         });
+        var html2 = await page.content();
         for(var classs in grades){
-          console.log(grades[classs]["title"]); 
+          console.log(grades[classs]["title"]);
           navresponse = page.waitForNavigation(['networkidle0', 'load', 'domcontentloaded']);
           console.log(navresponse)
-          page.click("span[title='"+grades[classs]["title"]+"']");
-          console.log("clicked")
+
+          console.log($("span[title='"+grades[classs]["title"]+"']",html2).text());
+          var res = page.click("span[title='"+grades[classs]["title"]+"']");
+
+          console.log("res")
+          await res;
+          console.log(res)
           await navresponse;
           console.log("response")
-          await page.screenshot({path: classs+'examples.png'});
+          //await page.screenshot({path: classs+'examples.png'});
           console.log("Going to grade book");
           navresponse = page.waitForNavigation(['networkidle0', 'load', 'domcontentloaded']);
           await page.evaluate(text => [...document.querySelectorAll('*')].find(e => e.textContent.trim() === text).click(), "Gradebook");
@@ -99,9 +111,12 @@ function func(){
           //await page.waitForNavigation({ waitUntil: 'networkidle2' })
           await navresponse;
           console.log(navresponse)
-          
+          html2 = page.content();
+          console.log("html2");
+          await page.screenshot({path: 'examples.png'});
+			await html2;
         }
-        
+
       }
     }
 
