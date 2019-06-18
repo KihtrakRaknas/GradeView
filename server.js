c8e9cfd69bdf1e77f9f7fb03df08ce2b471b0867
KihtrakRaknas <karthikandsankar@gmail.com>
2019-06-18 17:59:43 -0400

    bug fix

--- server.js
+++ server.js
@@ -282,7 +282,7 @@ async function checkUser(email,pass) {
         */
         });
       const page = await browser.newPage();
-  
+      await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3738.0 Safari/537.36');
       /*await page.setViewport({
         width: 1920,
         height: 1080
@@ -395,7 +395,7 @@ var url2 = 'https://students.sbschools.org/genesis/j_security_check?j_username='
       */
       });
     const page = await browser.newPage();
-
+    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3738.0 Safari/537.36');
 
     await page.setRequestInterception(true);
     const blockedResourceTypes = [
@@ -637,7 +637,7 @@ var url2 = 'https://students.sbschools.org/genesis/j_security_check?j_username='
           */
           });
         const page = await browser.newPage();
-    
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3738.0 Safari/537.36');
     
         await page.setRequestInterception(true);
         const blockedResourceTypes = [
@@ -786,7 +786,7 @@ var url2 = 'https://students.sbschools.org/genesis/j_security_check?j_username='
             */
             });
           const page = await browser.newPage();
-      
+          await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3738.0 Safari/537.36');
       
           await page.setRequestInterception(true);
           const blockedResourceTypes = [
